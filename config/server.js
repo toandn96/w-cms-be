c837aebb2f9dbf8e51674b93b2d5a7c684d96dfd
toandn <toandn96@gmail.com>
2021-10-05 14:21:35 +0700

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '55a61df2e951217b3690464fca6419ac'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'da8bf0ff-56eb-4313-852f-68cadc9ee6a9'
+};
